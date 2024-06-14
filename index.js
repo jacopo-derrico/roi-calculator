ab742663c8da0b72c2f3d8688982fae8fcdcc77f
jacopo-derrico <derricoj.design@gmail.com>
2024-06-14 17:15:55 +0200

    feat: add notches with values

--- index.js
+++ index.js
@@ -3,6 +3,21 @@ const btnAdd = document.getElementById('btnAdd');
 const btnDetract = document.getElementById('btnDetract');
 const finalPrice = document.getElementById('finalPrice');
 
+const notchesContainer = document.getElementById('notches-container');
+
+function createNotches(positions) {
+    positions.forEach(position => {
+        const notch = document.createElement('div');
+        notch.classList.add('notch');
+        notch.style.left = `calc(${position}% - 0.5px)`; // Adjust position as needed
+        notchesContainer.appendChild(notch);
+    });
+}
+
+// Define the positions for the six notches
+const notchPositions = [3, 9, 15, 24, 45, 70];
+createNotches(notchPositions);
+
 function roiCalculator(contracts) {
     finalPrice.textContent = contracts*20;
     console.log(contracts)
