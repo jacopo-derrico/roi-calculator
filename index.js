c489f2b7071d3dcaefce5f8dea973e6d7d51f89d
jacopo-derrico <derricoj.design@gmail.com>
2024-06-14 14:39:07 +0200

    feat: add function to buttons

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const priceRange = document.getElementById('priceRange');
-const btnAddaddBtn = document.getElementById('btnAdd');
+const btnAdd = document.getElementById('btnAdd');
 const btnDetract = document.getElementById('btnDetract');
 const finalPrice = document.getElementById('finalPrice');
 
@@ -10,6 +10,20 @@ function roiCalculator(contracts) {
 
 priceRange.addEventListener('input', function() {
     roiCalculator(priceRange.value);
+});
+
+btnDetract.addEventListener('click', () => {
+    if (priceRange.value > 9) {
+        priceRange.value = parseInt(priceRange.value) - 3;
+        roiCalculator(priceRange.value);
+    }
+});
+
+btnAdd.addEventListener('click', () => {
+    if (priceRange.value < 150) {
+        priceRange.value = parseInt(priceRange.value) + 3;
+        roiCalculator(priceRange.value);
+    }
 });
 
 roiCalculator(priceRange.value);
